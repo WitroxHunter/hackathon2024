b8a821c30ca4a049e7fc5b6465d27569dadb4901
Wiktor <lolxdyoloelo@gmail.com>
2024-03-16 01:04:54 +0100

    sdfvs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import hourglass from "./assets/hourglass.svg";
 import "./App.css";
 
 function App() {
@@ -9,8 +10,11 @@ function App() {
       <div id="main">
         <div id="phoneTop"></div>
         <div id="phoneContent" class="scrollable-div">
-          <h2 className="dzisuzywalesfona">Dziś korzystałes z telefonu </h2>
-          <h1 className="godzinyDuze">6h i 32min</h1>
+          <div id="phoneContentTopBar">
+            <h2 className="dzisuzywalesfona">Dziś korzystałeś z telefonu </h2>
+            <h1 className="godzinyDuze">6h i 32min</h1>
+            <img src={hourglass} className="hourglassImage"></img>
+          </div>
         </div>
         <div id="phoneBottom"></div>
       </div>
